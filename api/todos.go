5c66e25e153c101ed9e10d82f7a6c0a23ec77dee
Tolaz Hewa <tolazhewa@Tolazs-MacBook-Pro.local>
2024-04-15 20:49:31 -0400

    Added get todos endpoint

--- api/todos.go
+++ api/todos.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tolazhewa/todo-backend-go/db"
+	"github.com/tolazhewa/todo-backend-go/models"
 )
 
 func AddTodoRoutes(r *gin.RouterGroup) {
@@ -15,5 +18,30 @@ func AddTodoRoutes(r *gin.RouterGroup) {
 }
 
 func GetTodos(c *gin.Context) {
+	db := db.GetDB()
+	queryStatement := `SELECT * FROM todos`
+	rows, err := db.Query(queryStatement)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to retrieve todos"})
+		return
+	}
 
+	todos := make([]models.Todo, 0)
+	for rows.Next() {
+		var todo models.Todo
+ 		err = rows.Scan(&todo)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed getting todos"})
+			return
+		}
+		todos = append(todos, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed getting todos"})
+		return
+	}
+	
+	c.JSON(http.StatusOK, todos)
 }
